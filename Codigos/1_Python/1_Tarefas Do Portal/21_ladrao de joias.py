9f40baf28b7c8d8558551bd33922fca27d5bdb05
AceNobGames <acenobgames@gmail.com>
2025-10-10 17:30:19 -0300

    joias

--- Codigos/1_Python/1_Tarefas Do Portal/21_ladrao de joias.py
+++ Codigos/1_Python/1_Tarefas Do Portal/21_ladrao de joias.py
@@ -33,9 +33,7 @@ pistas = (
 
  '\nFim de Jogo: \n O suspeito foi capturado em uma emboscada, \n'+
  'pegamos ela com a esmeralda, em uma das trilhas do Safári.\n'
-
 )
-
 
 fala_jornal = """“Um colar de esmeraldas, que pertenceu à princesa Isabel, foi furtado do Museu Nacional Quinta da Boa Vista, localizado no Rio de Janeiro e recentemente recuperado de um incêndio. O furto ocorreu durante um evento internacional que, por meio da exposição das jóias da família real, tentava angariar fundos para a ampliação e restauração do acervo do museu. Por enquanto a polícia segue investigando o caso, mas os suspeitos não tiveram seus nomes divulgados."""
 
@@ -43,3 +41,39 @@ print(fala_jornal+
       suspeitos+
       pistas
       )
+
+homens  = set(); mulheres  = set()
+calvos  = set(); loiros    = set(); pretos  = set(); ruivos = set()
+altos   = set(); medios    = set(); baixos  = set()
+obesos = set(); sobrepesos = set(); normais = set(); magros = set()
+for s in suspeitos:
+
+ if s == suspeitos[0]:
+   s_f  = ''; s_m  = ''; 
+ if s[1] in 'F':
+   mulheres.update(s)
+   s_f += '| '+s[0]
+
+ else:
+   homens.update(s)
+   s_m += '| '+s[0]
+print(pistas[0])
+print('\nTodos os elementos ainda são suspeitos...')
+print(pistas[1])
+print('\nHomens descartados da lista de suspeitos, exibindo os dados das mulheres:\n')
+print(mulheres)
+print(pistas[2])
+print('\nExibindo os dados das mulheres que caberiam em um duto de ventilação:\n')
+print((mulheres & (normais | magros)))
+print(pistas[3])
+print('\nExibindo os dados das mulheres loiras que se encaixam nesse perfil:\n')
+mulheres_loiras = mulheres & loiros
+print('\n', (mulheres_loiras & (normais | magros )))
+print(pistas[4])
+print('\nNo momento, as duas mulheres ainda são suspeitas...')
+print(pistas[5])
+acusado = mulheres_loiras & ((normais | magros) & medios)
+print('\nDados do Acusado: \n', acusado)
+for i in suspeitos:
+ if i[0] in acusado:
+   print('\nEnviado um mandato de prisão para ', i[0])
