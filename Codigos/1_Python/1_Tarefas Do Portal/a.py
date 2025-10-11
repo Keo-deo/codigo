12c9774d2203d1e00d82fb79d063cb6d1d6b8a26
Murilo <murilonutels2012@gmail.com>
2025-10-11 10:22:37 -0300

    emotion

--- Codigos/1_Python/1_Tarefas Do Portal/a.py
+++ Codigos/1_Python/1_Tarefas Do Portal/a.py
@@ -1,7 +1,24 @@
-def star():
-  print("star wars é a melhor franquia do mundo")
-sw = star()
-def professor():
-  print("lucas")
-nome_do_professor = professor
-nome_do_professor()
+def somar(n1, n2):
+    global resultado
+    resultado = n1 + n2
+n1 = int(input("digite o primeiro numero: "))
+n2 = int(input("digite o segundo numero: "))
+somar(n1, n2)
+print("O resultado da soma é:", resultado)
+
+def mensagem(msg:str, emoticon = ' '):
+ '''
+ Essa função exibe uma mensagem para o usuário. Se desejar,
+ ela também pode exibir um emoticon.
+ '''
+
+ print(f'\n{msg}')
+
+  #Então por padrão a mensagem sempre será exibida sem um emoticon,
+
+  #Mas se o usuário inserir um valor diferente do padrão.
+ if emoticon != ' ':
+   print(f'\n{emoticon}') #Esse emoticon vai aparecer na tela.
+
+mensagem('Olá Mundo')
+mensagem('Tudo bem?', '😄')
